ea8ab4c90e8c3fd91e8b1276d5047fde5a1a4312
Eli <elireid.28@gmail.com>
2024-08-21 10:30:41 -0700

    basic character functionality complete + live feedback added

--- src/app/components/typing-space/typing-space.component.ts
+++ src/app/components/typing-space/typing-space.component.ts
@@ -1,65 +1,100 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-typing-space',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './typing-space.component.html',
   styleUrl: './typing-space.component.css'
 })
 export class TypingSpaceComponent {
 
-  letters:string[] = ['a', 'b', 'c', 'd','e', 'f', 'g', 'h','i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's','t','u','v','w','x','y','z']; 
+  letters: string[] = ['a', 'b', 'c', 'd','e', 'f', 'g', 'h','i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's','t','u','v','w','x','y','z']; 
 
-  numbers:string[] = ['0','1', '2', '3', '4', '5','6','7','8','9']
+  numbers: string[] = ['0','1', '2', '3', '4', '5','6','7','8','9']
 
-  symbols:string[] = [']', '[', '(',')','{','}','|','&','^','~','`','@','#','$','%',',','.','!','?',';',':','\'','\"','\\','/','-','+','*','=','<','>']
+  symbols: string[] = [']', '[', '(',')','{','}','|','&','^','~','`','@','#','$','%',',','.','!','?',';',':','\'','\"','\\','/','-','+','*','=','<','>']
 
-  preferences:string[] = this.letters;
+  words: string[] = ['cat', 'dog', 'horse', 'cow', 'pig']
+
+  correctStatus: boolean[] = [];
+
+  preferences: string[] = this.letters;
+
+  testArr: string[] = this.shuffleArray(this.preferences);
   
-  rand:number = Math.floor(Math.random() * 25);
+  rand: number = -1;
 
-  currChar:string = this.preferences[0];
+  currChar: string = this.preferences[0];
 
-  prevChar:string = "";
+  prevChar: string = "";
 
-  arrSize:number = this.preferences.length;
+  arrSize: number = this.preferences.length;
 
-  keysPressed:number = 0;
+  includedKeysPressed: number = 0;
 
-  correctKeys:number = 0;
+  excludedKeysPressed: number = 0;
 
-  correct:boolean = false;
+  totalKeysPressed: number = 0;
 
-  accuracy:number = 0;
+  correctKeys: number = 0;
+
+  correct: boolean = false;
+
+  accuracy: number = 0;
+
+  test: string = "test";
 
   getRandomInt():number {
     return this.rand = Math.floor(Math.random() * (this.arrSize));
   }
 
-  getRandomCharacter() {
-    this.currChar = this.preferences[this.rand]
+  getFirstCharacter() {
+    this.currChar = this.testArr[0];
   } 
 
   getNextChar() {
     this.prevChar = this.currChar;
-    this.getRandomInt();
-    this.getRandomCharacter();
+    this.currChar = this.testArr[this.includedKeysPressed]
   }
 
   onInput(event: any) {
-    const lastTypedChar: string = event.target.value.slice(-1);
-    if(this.preferences.includes(lastTypedChar)) {
-      this.keysPressed++;
+    const lastTypedChar: string = event.key;
+    if(!this.testArr.includes(lastTypedChar) && !(lastTypedChar == "Backspace" || lastTypedChar == " " || lastTypedChar == "Shift")) {
+      this.excludedKeysPressed++;
+    }
+    if(this.testArr.includes(lastTypedChar)) {
+      this.includedKeysPressed++;
       if (this.currChar == lastTypedChar) {
         this.correct = true;
         this.correctKeys++;
+        this.correctStatus.push(true);
       }
       else {
         this.correct = false;
+        this.correctStatus.push(false);
       }
-      this.accuracy = Number.parseFloat((this.correctKeys/this.keysPressed).toFixed(2));
-      this.getNextChar();
     }
+
+    this.calcAccuracy();
+    this.getNextChar();
+
   }
+
+  calcAccuracy(): number {
+    this.totalKeysPressed = this.includedKeysPressed + this.excludedKeysPressed;
+    return this.accuracy = Number.parseFloat((this.correctKeys/this.totalKeysPressed).toFixed(2));
+  }
+
+  shuffleArray(arr: string[]): string[] {
+    let currIndex: number = arr.length;
+    while(currIndex != 0) {
+      let randIndex: number = Math.floor(Math.random() * currIndex);
+      currIndex--;
+      [arr[currIndex], arr[randIndex]] = [arr[randIndex], arr[currIndex]];
+    }
+    return arr;
+  }
+
 }
