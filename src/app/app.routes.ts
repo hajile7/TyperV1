66260c6752e322620a76a3fc3dc51b72b5b14644
Eli <elireid.28@gmail.com>
2024-09-11 12:35:47 -0700

    Made some very minor changes. Added some symbols to the navbar instead of text + created profile component to implement at a later time

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { WordTypingSpaceComponent } from './components/word-typing-space/word-typing-space.component';
 import { TypingSpaceComponent } from './components/typing-space/typing-space.component';
+import { PreferencesComponent } from './components/preferences/preferences.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
     {path: "Home/Words", component: WordTypingSpaceComponent},
     {path: "Home/Characters", component: TypingSpaceComponent},
     {path: "Login", component: LoginComponent},
+    {path: "Settings", component: PreferencesComponent},
+    {path: "Profile", component: ProfileComponent},
     {path: "**", redirectTo: "Home/Words", pathMatch: "full"}
 ];
